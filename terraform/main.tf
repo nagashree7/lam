1ad9a61a3418448638da82eadaec1fc46b507473
nagashree7 <nagashree721@gmail.com>
2025-02-06 11:49:59 +0530

    lambda commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,28 @@
 provider "aws" {
   region = var.region
 }
- 
+
 # Define region variable
 variable "region" {
   description = "The AWS region to deploy resources into."
   type        = string
   default     = "us-east-1"
 }
- 
+
 # Get AWS account ID dynamically
 data "aws_caller_identity" "current" {}
- 
-# Create S3 bucket
-resource "aws_s3_bucket" "lambda_bucket" {
-  provider = aws 
-  bucket = "bucket-s3"
+
+# Create random pet for S3 bucket name
+resource "random_pet" "s3_bucket_name" {
+  length = 2  # Generates two words
+}
+
+# Create S3 bucket with random name
+resource "aws_s3_bucket" "my_bucket" {
+  bucket        = "my-unique-s3-bucket-${lower(random_pet.s3_bucket_name.id)}"  # Ensure the name is lowercase
   force_destroy = true
 }
- 
+
 # IAM Role for Lambda
 resource "aws_iam_role" "lambda_role" {
   name = "iam-role"
@@ -38,7 +42,10 @@ resource "aws_iam_role" "lambda_role" {
 EOF
 }
 
-resource "random_pet" "lambda_policy_suffix" {   length = 2 } 
+# Random pet for Lambda policy suffix
+resource "random_pet" "lambda_policy_suffix" {
+  length = 2
+}
 
 # IAM Policy for Lambda to Access S3 and CloudWatch Logs
 resource "aws_iam_policy" "lambda_policy" {
@@ -57,7 +64,7 @@ resource "aws_iam_policy" "lambda_policy" {
         "logs:PutLogEvents"
       ],
       "Resource": [
-        "${aws_s3_bucket.lambda_bucket.arn}/*",
+        "${aws_s3_bucket.my_bucket.arn}/*",  # Updated to use my_bucket ARN
         "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/*"
       ]
     }
@@ -65,40 +72,39 @@ resource "aws_iam_policy" "lambda_policy" {
 }
 EOF
 }
- 
+
 # Attach Policy to Lambda Role
 resource "aws_iam_role_policy_attachment" "lambda_s3_cloudwatch_attach" {
   policy_arn = aws_iam_policy.lambda_policy.arn
   role       = aws_iam_role.lambda_role.name
 }
- 
+
 # Lambda function
 resource "aws_lambda_function" "s3_lambda" {
   function_name = "Lambda-function"
   runtime       = "python3.8"
   handler       = "index.lambda_handler"
   role          = aws_iam_role.lambda_role.arn
- 
+
   filename         = "../lambda_function.zip"
   source_code_hash = filebase64sha256("../lambda_function.zip")
 }
- 
+
 # S3 Event Notification -> Lambda
 resource "aws_s3_bucket_notification" "s3_lambda_trigger" {
-  bucket = aws_s3_bucket.lambda_bucket.id
- 
+  bucket = aws_s3_bucket.my_bucket.id  # Updated to use my_bucket.id
+
   lambda_function {
     lambda_function_arn = aws_lambda_function.s3_lambda.arn
     events             = ["s3:ObjectCreated:*"]
   }
 }
- 
+
 # Allow S3 to invoke Lambda
 resource "aws_lambda_permission" "allow_s3" {
   statement_id  = "AllowExecutionFromS3"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.s3_lambda.function_name
   principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.lambda_bucket.arn
+  source_arn    = aws_s3_bucket.my_bucket.arn  # Updated to use my_bucket.arn
 }
-
